f18600fd00a801d0c9e207c0f756faa27a8d0cb5
earGO <rh.edwin@gmail.com>
2018-09-06 06:20:23 -0500

    app with simple components and no state

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import Card from './Card';
+import Card from './Card'
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
+    const cardComponent = robots.map((user,i)=>{
+    return (
+        <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            username={robots[i].username} 
+            email={robots[i].email} />);
+    });
+
+    return (
+        <div>
+            {cardComponent}
+        </div>
+    );
 }
 
-export default CardList;
+export default CardList; 
