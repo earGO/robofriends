02350d2fc57b714a3cb8ecb5924003b0d679d3cb
earGO <rh.edwin@gmail.com>
2018-09-06 07:53:38 -0500

    programmed states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import { robots } from './robots';
 
+const state = {
+  robots: robots,
+  searchfield: '',
 
-const App = () => {
-  return (
-    <div className = 'tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>  
-  );
+}
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: '',
+        }
+    }
+  
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className = 'tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>  
+    );
+  }
 }
 
 export default App;
